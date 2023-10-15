2d7052530e3f5cbdf3d2fbe9a771b50dedc7ba13
SanthoshJayaram1 <santhoshjayaram193@gmail.com>
2023-10-16 03:08:59 +0530

    update

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -13,6 +13,7 @@ module.exports.generateRefreshToken = (payload, res) => {
   const token = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '30d'})
  
   res.cookie('wechat_rfToken', token, {
+    secure:true,
     httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000,
     path: '/'
